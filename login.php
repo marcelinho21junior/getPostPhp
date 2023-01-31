e51ae8e90e1add479e16803948511afe4b1a4c66
Marcelo Junior <marcelo.junior@escoladnc.com.br>
2023-01-30 22:29:31 -0300

    first

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="recebe-login.php" method="POST">
+        <label for="">E-mail
+            <input type="text" name="email">
+        </label>
+        <label for="">Senha
+            <input type="password" name="senha">
+        </label>
+        <button type='submit'>Logar</button>
+    </form>
+</body>
+</html>
